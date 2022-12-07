9c39f199c50addef6d8ba7ac074e01a63f37db13
verbrcar000 <114618780+verbrcar000@users.noreply.github.com>
2022-12-07 12:02:55 -0700

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -19,3 +19,16 @@ INNER JOIN titles
 ON employees.emp_no = titles.emp_no
 WHERE employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY employees.emp_no;
+
+-- Find most recent title of each employee
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles
