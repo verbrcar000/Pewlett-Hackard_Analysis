a5920803d7b18fadd8e1b8cdd730b242ddf117d1
verbrcar000 <114618780+verbrcar000@users.noreply.github.com>
2022-12-07 13:10:21 -0700

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -41,3 +41,23 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles
+
+-- Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	titles.title
+INTO mentorship_eligiblity
+FROM employees as e
+JOIN dept_emp as de 
+ON e.emp_no = de.emp_no
+JOIN titles
+ON e.emp_no = titles.emp_no
+WHERE de.to_date = '9999-01-01'
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+select * from mentorship_eligiblity
